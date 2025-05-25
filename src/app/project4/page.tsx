1007d7e855005ff38198cdae6295e72ddd71b671
Youssuf Helaly <youssufhelaly@gmail.com>
2025-05-25 03:28:14 -0400

    project4

---
+++ src/app/project4/page.tsx
@@ -0,0 +1,15 @@
+// app/projet1/page.js
+export default function Projet4() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white px-4">
+      <h1 className="text-4xl font-semibold mb-4">Design 3 : E-commerce</h1>
+      <p className="text-gray-300 mb-6">Page à venir…</p>
+      <a
+        href="/"
+        className="inline-block px-4 py-2 border border-cyan-500 rounded hover:bg-cyan-700 transition"
+      >
+        ← Retour à l’accueil
+      </a>
+    </main>
+  );
+}
