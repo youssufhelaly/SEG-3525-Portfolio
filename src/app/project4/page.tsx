d8f5c5b2165de03a489d3d3e9ed45317db3cf9bb
Youssuf Helaly <youssufhelaly@gmail.com>
2025-05-25 03:36:23 -0400

    Link

--- src/app/project4/page.tsx
+++ src/app/project4/page.tsx
@@ -1,15 +1,17 @@
+import Link from "next/link";
+
 // app/projet1/page.js
 export default function Projet4() {
   return (
     <main className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white px-4">
       <h1 className="text-4xl font-semibold mb-4">Design 3 : E-commerce</h1>
       <p className="text-gray-300 mb-6">Page à venir…</p>
-      <a
+      <Link
         href="/"
         className="inline-block px-4 py-2 border border-cyan-500 rounded hover:bg-cyan-700 transition"
       >
         ← Retour à l’accueil
-      </a>
+      </Link>
     </main>
   );
 }
