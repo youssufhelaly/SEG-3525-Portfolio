27f8733320914018f4f69ba4df9a2479dfbfffb8
Youssuf Helaly <youssufhelaly@gmail.com>
2025-05-25 22:53:47 -0400

    remove footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 
         <main>{children}</main>
 
-        <footer className="text-center py-4 text-white bg-gray-900/85">
-          © 2025 Youssuf Helaly. Tous droits réservés.
-        </footer>
-
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
           strategy="afterInteractive"
